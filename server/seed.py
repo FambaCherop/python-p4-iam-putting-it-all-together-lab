08fbc864ae055fe8e527426ee950968bec3c44d6
laurahfamba <cheroplaurah7@gmail.com>
2024-07-14 17:14:57 +0300

    code

--- server/seed.py
+++ server/seed.py
@@ -19,7 +19,7 @@ with app.app_context():
 
     print("Creating users...")
 
-    # make sure users have unique usernames
+    
     users = []
     usernames = []
 
@@ -28,6 +28,8 @@ with app.app_context():
         username = fake.first_name()
         while username in usernames:
             username = fake.first_name()
+
+
         usernames.append(username)
 
         user = User(
@@ -46,11 +48,13 @@ with app.app_context():
     recipes = []
     for i in range(100):
         instructions = fake.paragraph(nb_sentences=8)
+
         
         recipe = Recipe(
             title=fake.sentence(),
             instructions=instructions,
             minutes_to_complete=randint(15,90),
+
         )
 
         recipe.user = rc(users)
@@ -58,6 +62,8 @@ with app.app_context():
         recipes.append(recipe)
 
     db.session.add_all(recipes)
+
+
     
     db.session.commit()
     print("Complete.")
